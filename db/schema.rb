18a85215838882d76a852cd7caf3b65796b09678
carojane <carojane@gmail.com>
2014-09-10 14:58:13 -0400

    added reader_id to checkouts"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140910184117) do
+ActiveRecord::Schema.define(version: 20140910185326) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20140910184117) do
     t.integer  "book_id",    null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "reader_id",  null: false
   end
 
   create_table "readers", force: true do |t|
